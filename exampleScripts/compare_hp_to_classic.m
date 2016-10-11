2c455929327addb3cf8bbf688bfe5cb532f4d922
Lizzie Lundgren <elundgren@seas.harvard.edu>
2016-10-11 18:05:24 -0400

    Functional improvements in compare_hp_to_classic.m
    Updates include:
    	(1) calculating mean, min, max for only the configured level
    	(2) set constant colorbar endpoints for a given species and
                run type (so cs, regridded ll, and classic lat/lon all
                have a common color mapping)
    	(3) Plot differences and ratios between two runs for a given data
                set (e.g. difference and ratio plots for drydep off vs on
                in GCHP)
    	(4) Plot differences and ratios between gchp regridded and
                classic for each species and run type (e.g. gchp / classic
                for O3 when dry dep is on).
    
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- exampleScripts/compare_hp_to_classic.m
+++ exampleScripts/compare_hp_to_classic.m
@@ -9,14 +9,13 @@
 clear all; close all;
 
 global CSGridDir
-format long
 
 %-------------------
 % Configurables
 %-------------------
 
 % Define species of interest (may include O3, NO, CO, NO2)
-spclist = {'O3', 'NO'};
+spclist = {'O3'};
 
 % Define level of interest, where index increases starting at the surface.
 % This must be scalar. NOTE: This requires GCHP data to be vertically 
@@ -37,7 +36,7 @@ cc_run_prefix = 'classic_4x5_trop_';
 % This corresponds to GCHP run directories hp_4x5_trop_std, 
 % hp_4x5_trop_noET, etc, and GC classic run directories classic_4x5_trop_std,
 % classic_4x5_trop_noET, etc. 
-run_suffix = {'std', 'noET', 'noETDD'};
+run_suffix = {'DDonly', 'allOff'};
 
 % Choose which model outputs to include. This is where you can isolate
 % comparing only GCHP, or only classic.
@@ -48,7 +47,7 @@ classic_on          = true;
 % Choose whether to plot data (will plot all runs, all species, at level lev)
 % NOTE: this can be slow. Best compare without plots first to look
 % at metrics.
-plots_on = false;
+plots_on = true;
 
 % Choose what metrics to compare between runs. Currently min, max, or mean.
 metric = 'mean';
@@ -90,7 +89,7 @@ if classic_on;          cc    = struct; end % GC classic output
 h = 1;
 
 %------------------------------------------------------
-% Loop over runs and species to read/set data and plot
+% Loop over runs and species to read in data
 %------------------------------------------------------
 for i =1:length(run_suffix);
   
@@ -113,18 +112,9 @@ for i =1:length(run_suffix);
       hp_cs.(run).(spc)      = struct;
       hp_cs.(run).(spc).data = ncread(fpath_hp_cs{i}, ['TRC_', spc]);
       hp_cs.(run).(spc).data = flip(hp_cs.(run).(spc).data,3);
-      hp_cs.(run).(spc).min  = min(hp_cs.(run).(spc).data(:));
-      hp_cs.(run).(spc).max  = max(hp_cs.(run).(spc).data(:));
-      hp_cs.(run).(spc).mean = mean(hp_cs.(run).(spc).data(:));
-      if plots_on;
-         figure(h)
-         titletext = 'GCHP cubed-sphere';
-         plotCSLayer(hp_cs.(run).(spc).data(:,:,lev),'projection','miller');
-         colorbar;
-         %caxis([spc.O3.min spc.O3.max]);
-         % NOTE: title not currently possible plotCSLayer?
-         h = h + 1;
-      end
+      hp_cs.(run).(spc).min  = min(min(hp_cs.(run).(spc).data(:,:,lev)));
+      hp_cs.(run).(spc).max  = max(max(hp_cs.(run).(spc).data(:,:,lev)));
+      hp_cs.(run).(spc).mean = mean(mean(hp_cs.(run).(spc).data(:,:,lev)));
     end  
 
     % GCHP runs, lat/lon output
@@ -132,38 +122,108 @@ for i =1:length(run_suffix);
       hp_ll.(run).(spc)      = struct;
       hp_ll.(run).(spc).data = ncread(fpath_hp_ll{i}, ['TRC_', spc]);
       hp_ll.(run).(spc).data = flip(hp_ll.(run).(spc).data,3);      
-      hp_ll.(run).(spc).min  = min(hp_ll.(run).(spc).data(:));
-      hp_ll.(run).(spc).max  = max(hp_ll.(run).(spc).data(:));
-      hp_ll.(run).(spc).mean = mean(hp_ll.(run).(spc).data(:));
-      if plots_on;
-        figure(h)
-        plotGrid(hp_ll.(run).(spc).data(:,:,lev), ...
-        	       'layer','projection','miller');
-        colorbar;
-        %caxis([spc.O3.min spc.O3.max]);
-        title(['GCHP 4x5 regridded, GC level ', num2str(lev), ...
-        	     ', ', spc, ', ', run]);
-        h = h + 1;
-      end
+      hp_ll.(run).(spc).min  = min(min(hp_ll.(run).(spc).data(:,:,lev)));
+      hp_ll.(run).(spc).max  = max(max(hp_ll.(run).(spc).data(:,:,lev)));
+      hp_ll.(run).(spc).mean = mean(mean(hp_ll.(run).(spc).data(:,:,lev)));
     end  
 
     % GEOS-Chem classic runs
     if classic_on;
       cc.(run).(spc)      = struct;      
       cc.(run).(spc).data = ncread(fpath_cc{i}, ['TRACER_CONC_', spc]);
-      cc.(run).(spc).min  = min(cc.(run).(spc).data(:));
-      cc.(run).(spc).max  = max(cc.(run).(spc).data(:));
-      cc.(run).(spc).mean = mean(cc.(run).(spc).data(:));
-      if plots_on;
+      cc.(run).(spc).min  = min(min(cc.(run).(spc).data(:,:,lev)));
+      cc.(run).(spc).max  = max(max(cc.(run).(spc).data(:,:,lev)));
+      cc.(run).(spc).mean = mean(mean(cc.(run).(spc).data(:,:,lev)));
+    end  
+  end
+end
+
+%---------------------------------------------------------
+% Get min and max for all runs per species for comparison
+%---------------------------------------------------------
+cminval = zeros(1,length(spclist));
+cmaxval = zeros(1,length(spclist));
+for i =1:length(run_suffix);
+  run = run_suffix{i};
+  for j = 1:length(spclist);
+    spc = spclist{j};
+    cminval(j)=1e30;
+    cmaxval(j)=1e-30;
+
+    if gchp_cubedsphere_on;
+      if hp_cs.(run).(spc).min < cminval(j);
+	cminval(j) = hp_cs.(run).(spc).min;
+      end
+      if hp_cs.(run).(spc).max > cmaxval(j);
+	cmaxval(j) = hp_cs.(run).(spc).max;
+      end
+    end  
+  
+    % GCHP runs, lat/lon output
+    if gchp_latlon_on;
+      if hp_ll.(run).(spc).min < cminval(j);
+        cminval(j) = hp_ll.(run).(spc).min;
+      end
+      if hp_ll.(run).(spc).max > cmaxval(j);
+        cmaxval(j) = hp_ll.(run).(spc).max;
+      end
+    end  
+  
+    % classic runs
+    if classic_on;
+      if cc.(run).(spc).min < cminval(j);
+        cminval(j) = cc.(run).(spc).min;
+      end
+      if cc.(run).(spc).max > cmaxval(j);
+        cmaxval(j) = cc.(run).(spc).max;
+      end
+    end 
+  end
+end
+
+%------------------------------------------------------
+% Loop over runs and species and plot
+%------------------------------------------------------
+if plots_on;
+  for i =1:length(run_suffix);
+    run = run_suffix{i};
+    for j = 1:length(spclist);
+      spc = spclist{j};
+  
+      % GCHP runs, cubed sphere output
+      if gchp_cubedsphere_on;
+        figure(h)
+        titletext = 'GCHP cubed-sphere';
+        plotCSLayer(hp_cs.(run).(spc).data(:,:,lev),'projection','miller');
+        colorbar;
+        caxis([cminval(j) cmaxval(j)]);
+        % NOTE: title not currently possible plotCSLayer?
+        h = h + 1;
+      end  
+  
+      % GCHP runs, lat/lon output
+      if gchp_latlon_on;
+        figure(h)
+        plotGrid(hp_ll.(run).(spc).data(:,:,lev), ...
+        	       'layer','projection','miller');
+        colorbar;
+        caxis([cminval(j) cmaxval(j)]);
+        title(['GCHP 4x5 regridded, GC level ', num2str(lev), ...
+        	     ', ', spc, ', ', run]);
+        h = h + 1;
+      end  
+  
+      % classic runs
+      if classic_on;
         figure(h)
         plotGrid(cc.(run).(spc).data(:,:,lev),'layer','projection','miller');
         colorbar;
-        %caxis([spc.O3.min spc.O3.max]);
-        title(['GEOS-Chem 4x5, GC level ', num2str(lev), ...
+        caxis([cminval(j) cmaxval(j)]);
+        title(['Classic 4x5, GC level ', num2str(lev), ...
         	     ', ', spc, ', ', run]);
         h = h + 1;
-      end
-    end  
+      end  
+    end
   end
 end
 
@@ -177,14 +237,107 @@ for i = 1:length(spclist);
     run = run_suffix{j};
     fprintf(' Run %s\n',run); 
     if gchp_cubedsphere_on; 
-      fprintf('   cubed sphere: %12.12d\n',hp_cs.(run).(spc).(metric)); 
+      fprintf('   cubed sphere: %12.8d\n',hp_cs.(run).(spc).(metric)); 
     end
     if gchp_latlon_on;      
-      fprintf('   cs regridded: %12.12d\n',hp_ll.(run).(spc).(metric)); 
+      fprintf('   cs regridded: %12.8d\n',hp_ll.(run).(spc).(metric)); 
     end
     if classic_on;          
-      fprintf('   classic:      %12.12d\n',cc.(run).(spc).(metric)); 
+      fprintf('   classic:      %12.8d\n',cc.(run).(spc).(metric)); 
     end
+  end
+end
+
+%---------------------------------------------------------------- 
+% Get difference of HP (regridded) and classic per spc/run combo
+%----------------------------------------------------------------
+if gchp_latlon_on && classic_on;
+  for i =1:length(run_suffix);
+    run = run_suffix{i};
+    for j = 1:length(spclist);
+      spc = spclist{j};
+
+      % calculate data difference and ratio
+      datadiff = hp_ll.(run).(spc).data(:,:,lev) ...
+                 - cc.(run).(spc).data(:,:,lev);
+      dataratio = hp_ll.(run).(spc).data(:,:,lev) ...
+                  ./cc.(run).(spc).data(:,:,lev);
+  
+      % plot the difference
+      if plots_on;
+        figure(h)
+        plotGrid(datadiff,'layer','projection','miller');
+        title([run ': HP - classic, level ', ...
+	       num2str(lev), ', ', spc]);
+        colorbar;
+        h = h + 1;
+      end  
+
+      % plot the ratio
+      if plots_on;
+        figure(h)
+        plotGrid(dataratio,'layer','projection','miller');
+        title([run ': HP / classic, level ', ...
+	       num2str(lev), ', ', spc]);
+        colorbar;
+        h = h + 1;
+      end  
+    end
+  end
+end
+
+%----------------------------------------------------
+% Get HP differences of run 1 vs run2 per species
+%----------------------------------------------------
+run1 = run_suffix{1};
+run2 = run_suffix{2};
+for j = 1:length(spclist);
+  spc = spclist{j};
+
+  % gchp lat/lon (regridded0
+  if gchp_latlon_on;
+     datadiff = hp_ll.(run1).(spc).data(:,:,lev) ...
+                - hp_ll.(run2).(spc).data(:,:,lev);
+     dataratio = hp_ll.(run1).(spc).data(:,:,lev) ...
+                 ./hp_ll.(run2).(spc).data(:,:,lev);
+     if plots_on;
+       figure(h)
+       plotGrid(datadiff,'layer','projection','miller');
+       title(['HP: ', run1, ' - ', run2, ...
+     	     ', level ', num2str(lev), ', ', spc]);
+       colorbar;
+       h = h + 1;
+
+       figure(h)
+       plotGrid(dataratio,'layer','projection','miller');
+       title(['HP: ', run1, ' / ', run2, ...
+     	     ', level ', num2str(lev), ', ', spc]);
+       colorbar;
+       h = h + 1;
+     end
+  end  
+
+  % classic
+  if classic_on;
+     datadiff = cc.(run1).(spc).data(:,:,lev) ...
+                - cc.(run2).(spc).data(:,:,lev);
+     dataratio = cc.(run1).(spc).data(:,:,lev) ...
+                 ./cc.(run2).(spc).data(:,:,lev);
+     if plots_on;
+       figure(h)
+       plotGrid(datadiff,'layer','projection','miller');
+       title(['Classic: ', run1, ' - ', run2, ...
+     	     ', level ', num2str(lev), ', ', spc]);
+       colorbar;
+       h = h + 1;
+
+       figure(h)
+       plotGrid(dataratio,'layer','projection','miller');
+       title(['Classic: ', run1, ' / ', run2, ...
+     	     ', level ', num2str(lev), ', ', spc]);
+       colorbar;
+       h = h + 1;
+     end 
   end
 end
 
